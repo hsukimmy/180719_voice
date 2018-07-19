9c45cd51f713fe1636cb177b554134790b707cad
Ｃloud_Bai <cloud_bai@loud-baide-macbook-pro.local>
2018-07-19 21:32:47 +0800

    Initial Commit

---
+++ 180719_voice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  180719_voice
+//
+//  Created by Ｃloud_Bai on 2018/7/19.
+//  Copyright © 2018年 Kimmy Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
